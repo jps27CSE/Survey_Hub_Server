17bf752c9d00a18c27a746e442c03ba2c4cfe16b
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-29 02:56:17 +0600

    Final

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 //middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://surveyhub-f382a.web.app",
+      "https://surveyhub-f382a.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
